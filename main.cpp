80a62a09f258fa50d4e3d5ba391211fadc394e69
zhufucdev <1095291434@qq.com>
2024-05-10 11:03:21 +0800

    :sparkles: impl

--- main.cpp
+++ main.cpp
@@ -2,6 +2,41 @@
 // Created by Reed Steve on 2024/3/9.
 //
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+bool solve() {
+    int n;
+    if (!(cin >> n)) {
+        return false;
+    }
+    int cap[n];
+    int max_idx = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> cap[i];
+        if (cap[i] >= cap[max_idx]) {
+            max_idx = i;
+        }
+    }
+    int other_cap = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i == max_idx) continue;
+        other_cap += cap[i];
+    }
+    if (cap[max_idx] > other_cap) {
+        cout << fixed << setprecision(1) << other_cap * 1.0;
+    } else {
+        cout << fixed << setprecision(1) << fixed << (cap[max_idx] + other_cap) * 0.5;
+    }
+
+    return true;
+}
+
 int main() {
+    while (solve()) {
+        cout << endl;
+    }
     return 0;
 }
