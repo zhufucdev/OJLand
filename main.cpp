729e8e48bc960e145e8da3aa9f95ca066ad15dc0
zhufucdev <1095291434@qq.com>
2024-06-07 10:27:41 +0800

    :ambulance: no idea

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 //
 // Created by Reed Steve on 2024/3/9.
 //
+#include <iostream>
+using namespace std;
+const int columns = 4;
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int col = 0; col < columns; ++col) {
+
+    }
+}
 
 int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; ++i) {
+        solve();
+    }
     return 0;
 }
