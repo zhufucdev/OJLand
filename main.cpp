b2e433e2f3c530e980808e1a60977785d39b24ac
zhufucdev <1095291434@qq.com>
2024-06-14 13:35:46 +0800

    :sparkles: copied from somewhere

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
-//
-// Created by Reed Steve on 2024/3/9.
-//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int d[400000];
+
+int distance(int n) {
+    for (int i = 0; i < n; i++) {
+        if (pow(2, i) == n)
+            return 0;
+        else if (pow(2, i) > n)
+            return (int) (n - pow(2, i - 1) + 1);
+    }
+    return -1;
+}
 
 int main() {
-    return 0;
+    int n, k, j = 0, result = 0, t;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        if (t == k)
+            d[j++] = distance(i + 1);
+    }
+    for (int i = 0; i < j; i++)
+    {
+        if (!d[i] || d[i] < result) {
+            result++;
+        } else {
+            result = max(result, d[i]);
+        }
+    }
+    cout << result << endl;
 }
