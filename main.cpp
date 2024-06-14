1e35aa97242ddecbe022db6f1fb80871cde65be3
zhufucdev <1095291434@qq.com>
2024-06-14 11:26:34 +0800

    :sparkles: copied from gh

--- main.cpp
+++ main.cpp
@@ -1,79 +1,69 @@
-//
-// Created by Reed Steve on 2024/3/9.
-//
 #include <iostream>
+#include <queue>
 
 using namespace std;
+int v[100][100];
 
-#define set_two(mat, x, y, m, n) \
-if (x >= 0 && x < m && y >= 0 && y < n && (!mat[y][x] || mat[y][x] == -1)) { \
-    mat_next[y][x] = 2;     \
-    any_change = true; \
-} \
+class Point {
+public:
+    int x, y;
 
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+queue<Point> myqueue;
+
+int least_instructions(int x, int y, int X, int Y, int N, int M) {
+    int move[4][2] = {{1,  0},
+                      {-1, 0},
+                      {0,  1},
+                      {0,  -1}};
+
+
+    myqueue.emplace(x, y);
+    v[x][y] = -1;
+    int length = 0;
+
+    while (!myqueue.empty()) {
+        auto size = myqueue.size();
+        for (int i = 0; i < size; i++) {
+            Point front = myqueue.front();
+            myqueue.pop();
+
+            if (front.x == X && front.y == Y) {
+                return length;
+            }
+
+            for (auto &j: move) {
+                int newX = front.x + j[0];
+                int newY = front.y + j[1];
+
+                if (newX > 0 && newX <= N && newY > 0 && newY <= M && v[newX][newY] == 0) {
+                    myqueue.emplace(newX, newY);
+                    v[newX][newY] = -1;
+                }
+            }
+        }
+        length++;
+    }
+
+    return -1;
+}
 
 int main() {
-    int n, m;
-    cin >> n >> m;
+    int N, M;
+    cin >> N >> M;
 
-    int **mat = new int *[n];
-    for (int i = 0; i < n; ++i) {
-        mat[i] = new int[m];
-        for (int j = 0; j < m; ++j) {
-            cin >> mat[i][j];
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            cin >> v[i][j];
         }
     }
-
-    int cx, cy, wx, wy;
-    cin >> cx >> cy >> wx >> wy;
-    cx--;
-    cy--;
-    wx--;
-    wy--;
-
-    mat[cy][cx] = 2;
-    mat[wy][wx] = -1;
-
-    int commands_count = 0;
-
-    bool any_change;
-    while (true) {
-        any_change = false;
-        int **mat_next = new int *[n];
-        mat_next[0] = new int[m];
-
-        for (int y = 0; y < n; ++y) {
-            if (y < n - 1) {
-                mat_next[y + 1] = new int[m];
-            }
-            for (int x = 0; x < m; ++x) {
-                if (mat[y][x] == 2) {
-                    set_two(mat, x - 1, y, m, n)
-                    set_two(mat, x + 1, y, m, n)
-                    set_two(mat, x, y + 1, m, n)
-                    set_two(mat, x, y - 1, m, n)
-                }
-                if (mat[y][x]) {
-                    mat_next[y][x] = mat[y][x];
-                }
-            }
-        }
-
-        commands_count++;
-
-        if (mat_next[wy][wx] == 2 || !any_change) {
-            delete[]mat_next;
-            break;
-        }
-        delete[]mat;
-        mat = mat_next;
-    }
-
-    if (any_change) {
-        cout << commands_count;
-    } else {
-        cout << -1;
-    }
-
-    return 0;
+    int x, y, X, Y;
+    cin >> x >> y >> X >> Y;
+    int len = least_instructions(x, y, X, Y, N, M);
+    cout << len;
 }
