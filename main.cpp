ebc98af2f264cafe89bd6ff555387f1e08922ad9
zhufucdev <1095291434@qq.com>
2024-06-18 11:35:20 +0800

    :sparkles: aigen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-//
-// Created by Reed Steve on 2024/3/9.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int longest(int n, vector<int> &a) {
+    if (n == 0) return 0;
+
+    vector<int> dp(n, 1);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (gcd(a[i], a[j]) == 1) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return *max_element(dp.begin(), dp.end());
+}
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    int result = longest(n, a);
+    cout << result << endl;
+
     return 0;
 }
