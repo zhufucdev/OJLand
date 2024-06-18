537b0389d95b6ea801fee40953a30c6261d52ddf
zhufucdev <1095291434@qq.com>
2024-06-18 10:02:32 +0800

    :sparkles: copied from cnblogs

--- main.cpp
+++ main.cpp
@@ -2,22 +2,36 @@
 // Created by Reed Steve on 2024/3/9.
 //
 #include <iostream>
+#include <map>
+
 using namespace std;
-const int columns = 4;
-
-void solve() {
-    int n;
-    cin >> n;
-    for (int col = 0; col < columns; ++col) {
-
-    }
-}
+int A[2000], B[2000], C[2000], D[2000];
 
 int main() {
-    int T;
+    int T, n;
     cin >> T;
-    for (int i = 0; i < T; ++i) {
-        solve();
+    for (int t = 0; t < T; ++t) {
+        int zeros = 0, sum;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> A[i] >> B[i] >> C[i] >> D[i];
+        }
+        map<int, int> result;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum = A[i] + B[j];
+                ++result[sum];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum = C[i] + D[j];
+                if (result[-sum] > 0) {
+                    zeros += result[-sum];
+                }
+            }
+        }
+        cout << zeros << endl;
     }
     return 0;
 }
