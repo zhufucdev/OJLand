2de160d6e975fdc29c6a819b9fdd37cd823bfed3
zhufucdev <1095291434@qq.com>
2024-06-18 15:48:48 +0800

    :poop: chatgpt's fix, still RE

--- main.cpp
+++ main.cpp
@@ -2,37 +2,38 @@
 // Created by Reed Steve on 2024/3/9.
 //
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 #define value_at(x, y) x >= 0 && x < n && y >= 0 && y < n && map[y][x] != 'W' ? dp[y][x] : 0
 
 bool has_solution(int m, int n) {
-    string map[m];
-    auto **dp = new int *[m];
+    vector<string> map(m);
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+
     for (int i = 0; i < m; ++i) {
-        dp[i] = new int[n];
-        for (int j = 0; j < n; ++j) {
-            dp[i][j] = 0;
-        }
         cin >> map[i];
     }
+
     dp[0][0] = 3;
     while (true) {
-        auto **dp_next = new int *[m];
+        vector<vector<int>> dp_next(m, vector<int>(n, 0));
         bool change = false;
+
         for (int i = 0; i < m; ++i) {
-            dp_next[i] = new int[n];
             for (int j = 0; j < n; ++j) {
                 if (map[i][j] == 'W') {
                     continue;
                 }
-                auto east = value_at(j + 1, i),
-                        south = value_at(j, i + 1),
-                        west = value_at(j - 1, i),
-                        north = value_at(j, i - 1);
-                auto max_surrounding = max(max(east, south), max(west, north));
-                auto next = map[i][j] == 'X' ? max_surrounding - 1 : max_surrounding;
+                int east = value_at(j + 1, i);
+                int south = value_at(j, i + 1);
+                int west = value_at(j - 1, i);
+                int north = value_at(j, i - 1);
+                int max_surrounding = max({east, south, west, north});
+                int next = map[i][j] == 'X' ? max_surrounding - 1 : max_surrounding;
+
                 if (next > dp[i][j]) {
                     dp_next[i][j] = next;
                     change = true;
@@ -42,12 +43,9 @@ bool has_solution(int m, int n) {
             }
         }
 
-        delete[] dp;
         dp = dp_next;
         if (!change || dp[m - 1][n - 1] > 0) {
-            auto reachable = dp[m - 1][n - 1] > 0;
-            delete[] dp;
-            return reachable;
+            return dp[m - 1][n - 1] > 0;
         }
     }
 }
