b6eb4250e7f4c71e1b3e4c3ef611c9e760ccbdbb
zhufucdev <1095291434@qq.com>
2024-06-18 11:15:48 +0800

    :sparkles: copied from cnblog

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 // Created by Reed Steve on 2024/3/9.
 //
 
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+int w[200000], s[200000];
+
 int main() {
+    int n, q, sum = 0;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+        sum += w[i];
+        w[i] = sum;
+    }
+    for (int i = 0; i < q; i++) {
+        cin >> s[i];
+    }
+    for (int i = 0; i < q; i++) {
+        int *p = lower_bound(w, w + n , s[i]) + 1;
+        cout << p - w << " ";
+    }
     return 0;
 }
