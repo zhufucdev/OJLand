6090da2fdd627c8ffe69aec5adffe6386f49d251
zhufucdev <1095291434@qq.com>
2024-06-18 10:20:11 +0800

    :sparkles: copied from cnblogs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-//
-// Created by Reed Steve on 2024/3/9.
-//
+#include <iostream>
+
+using namespace std;
+#define N 100000
 
 int main() {
-    return 0;
+    int n, m, dp[N], x[N], p[N];
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> p[i] >> x[i];
+    int s = 0;
+    if (p[0] > 0)
+        s = p[0];
+    dp[0] = p[0] + x[0];
+    for (int i = 0; i < n; i++) {
+        if (dp[i] < p[i + 1]) {
+            s += p[i + 1] - dp[i];
+            dp[i + 1] = p[i + 1] + x[i + 1];
+        } else {
+            if (dp[i] < p[i + 1] + x[i + 1]) {
+                dp[i + 1] = p[i + 1] + x[i + 1];
+            } else {
+                dp[i + 1] = dp[i];
+            }
+        }
+    }
+    if (dp[n - 1] < m) {
+        s += m - dp[n - 1];
+    }
+    cout << s << endl;
 }
