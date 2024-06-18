538b2ff877b58d877913d04d2c2c80f8daba2b8b
zhufucdev <1095291434@qq.com>
2024-06-18 11:24:04 +0800

    :sparkles: aigen

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 // Created by Reed Steve on 2024/3/9.
 //
 
+#include <iostream>
+
 int main() {
+    int X;
+    std::cin >> X;
+
+    int tens = X / 10;
+    int ones = X % 10;
+
+    int total_bills = tens + ones;
+    std::cout << total_bills << std::endl;
     return 0;
 }
